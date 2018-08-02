842878c1fcc69a3d2d0278b6ffd5269508c1b81b
Raúl Doezón <santor3medio@gmail.com>
2018-08-02 17:45:30 -0500

    First commit to project-camposycampos.

---
+++ src/js/menu.js
@@ -0,0 +1,78 @@
+var last_known_scroll_position = 0;
+var ticking = false;
+
+var siteNavbar = document.querySelector('.nav');
+
+//navbar-fixed-top
+function doSomething(scroll_pos) {
+  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-camposycampos/index.html' || window.location.pathname === '/project-camposycampos/') {
+    if (scroll_pos > 30) {
+      siteNavbar.classList.add('navbar-scroll');
+
+    } else {
+      siteNavbar.classList.remove('navbar-scroll');
+    }
+  }
+}
+
+doSomething();
+
+window.addEventListener('scroll', function(e) {
+  if (e.view) {
+    last_known_scroll_position = e.view.pageYOffset;
+  }
+  else {
+    last_known_scroll_position = window.scrollY;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false
+    });
+  }
+  ticking = true;
+});
+
+function showMenu() {
+  document.querySelector('#menuContainer').style.width = '85%';
+  document.querySelector('#hamburgerMenu').style.display = 'flex';
+}
+
+function hideMenu() {
+  document.querySelector('#menuContainer').style.width = '0%';
+  document.querySelector('#hamburgerMenu').style.display = 'flex';
+}
+
+window.addEventListener('click', function(e) {
+  if (!e.target.dataset.hasOwnProperty('attribute')) {
+    hideMenu();
+  }
+});
+
+/*Events of touchstart to mobile version*/
+
+var showHamburger = document.querySelector('#hamburgerMenu');
+
+showHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+var hideHamburger = document.querySelector('#hideHamburger');
+
+hideHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  hideMenu();
+});
+
+/* Events of click to desktop version*/
+
+showHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+hideHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  hideMenu();
+});

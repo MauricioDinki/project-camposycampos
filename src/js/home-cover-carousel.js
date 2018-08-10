dac4b348b8db103fe0e787f1f8d8d71dd27f3c24
Raúl Doezón <santor3medio@gmail.com>
2018-08-10 10:08:49 -0500

    #1. Add functionality to the home cover carousel. Add in home-cover-carousel.js

---
+++ src/js/home-cover-carousel.js
@@ -0,0 +1,24 @@
+var homeCoverCarousel = $('.home-cover-carousel');
+
+homeCoverCarousel.owlCarousel ({
+  loop: false,
+  nav: true,
+  dots: true,
+  navText: ['<img class="carousel-arrow" src="/img/icons/icon-arrow-left.png" />', '<img class="carousel-arrow" src="/img/icons/icon-arrow-right.png" />'],
+  margin: 20,
+  responsive:{
+    0:{
+      items:1
+    },
+    800:{
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+homeCoverCarousel.each(function(index) {
+  $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='home-cover-carousel-controls'></div>");
+});

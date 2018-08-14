c7fae48f2ce3c332194847c32904f5b9ddd1115e
Raúl Doezón <santor3medio@gmail.com>
2018-08-14 12:18:18 -0500

    #13. Add the integration url and contact form name propertie. Changes in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-$('form[name="contact-form"]').validate({
+$('form[name="camposycampos-contact-form"]').validate({
   name: 'required',
   email: 'required',
   phone: 'required',
@@ -8,17 +8,17 @@ $('form[name="contact-form"]').validate({
    phone: "Por favor, introduce tu Teléfono."
   },
   submitHandler: function(form) {
-   var data = $('form[name="contact-form"]').serialize();
+   var data = $('form[name="camposycampos-contact-form"]').serialize();
    console.log(data);
    $.ajax({
-     url: 'http://integrations.blick.mx/',
+     url: 'http://integrations.blick.mx/camposycampos/contact/',
      method: 'POST',
      data: data
    }).done(function(data) {
      if (parseInt(data) === 1) {
        alertify.logPosition("bottom right");
        alertify.success("Correo enviado, gracias por contactarte con nosotros.");
-       $('form[name="contact-form"]')[0].reset();
+       $('form[name="camposycampos-contact-form"]')[0].reset();
      }
    });
   },

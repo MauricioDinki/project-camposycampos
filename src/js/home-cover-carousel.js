2fe109e767021bf09076bea3e6a3b044248e7f68
Raúl Doezón <santor3medio@gmail.com>
2018-11-23 12:18:31 -0600

    #25. Include a new propertie navContainer and change the value of autoplayTimeout to 4000 for the home cover carousel. Changes in home-cover-carousel.js

--- src/js/home-cover-carousel.js
+++ src/js/home-cover-carousel.js
@@ -3,10 +3,11 @@ var homeCoverCarousel = $('.home-cover-carousel');
 homeCoverCarousel.owlCarousel ({
   loop: true,
   nav: true,
+  navContainer: '.home-cover-slide',
   dots: true,
   autoHeight: true,
   autoplay: true,
-  autoplayTimeout: 3000,
+  autoplayTimeout: 4000,
   navText: ['<img class="carousel-arrow" src="/img/icons/icon-arrow-left.png" />', '<img class="carousel-arrow" src="/img/icons/icon-arrow-right.png" />'],
   margin: 20,
   responsive:{
@@ -20,8 +21,4 @@ homeCoverCarousel.owlCarousel ({
       items: 1
     }
   }
-});
-
-homeCoverCarousel.each(function(index) {
-  $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='home-cover-carousel-controls'></div>");
 });
